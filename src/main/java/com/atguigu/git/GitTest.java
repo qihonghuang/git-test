02460b8c1de2ea900a2f7e7410bd25f686876cd6
qihonghuang <105647491+qihonghuang@users.noreply.github.com>
2022-05-18 23:05:29 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git master!");
         System.out.println("hello git hot-fix conflic!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
